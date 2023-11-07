830fed1410ca2b9be1f042f75c1760eb4c92a3cb
IvanTarasovHSE <144215666+IvanTarasovHSE@users.noreply.github.com>
2023-11-07 18:39:30 +0300

    Create function.js

---
+++ function.js
@@ -0,0 +1,17 @@
+const node_for_click = document.getElementById("for_click")
+
+function find_edit(){
+    const Name  = document.getElementsByTagName('span')[6]
+    console.log(Name.innerHTML)
+    Name.innerHTML = 'Ivan'
+
+    const surName  = document.getElementsByTagName('span')[5]
+    console.log(surName.innerHTML)
+    surName.innerHTML = 'Tarasov'
+
+
+}
+node_for_click.addEventListener("click",find_edit)
+
+
+
